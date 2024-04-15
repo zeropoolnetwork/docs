ef307a97fe758459619d3fd44a80764920ce68b4
snjax <igor.gulamov@gmail.com>
2024-04-16 01:47:05 +0400

    Updated CSS and documentation links
    Increased the minimum height in the CSS file for better layout handling. Also, swapped the positions of 'Privacy Engine' and 'Sharded Storage' sections on the documentation page to improve user navigation. The content within these sections has been updated accordingly to match their new titles.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import logo from '/static/img/ZEROpool_NEW_logo_wh.png'
-import Github from '/static/img/Github-icon.svg'
-import Medium from '/static/img/Medium.svg'
-import Telegram from '/static/img/Telegram.svg'
-import Twitter from '/static/img/Twitter.svg'
+
 
 const Footer = () => {
   return (
     <footer className="main-footer" id="contacts">
       <div className="main-container main-footer__container">
         <a className="main-footer__logo-link" href="/">
-          <img className="main-footer__logo" src={logo} alt="zeropool"/>
+          <img className="main-footer__logo" src='/img/ZEROpool_NEW_logo_wh.png' alt="zeropool"/>
         </a>
         <div className="main-footer__social">
           <a className="main-footer__social-link" href="https://github.com/zeropoolnetwork">
-            <Github className="main-footer__social-img" />
+            <img className="main-footer__social-img" src='/img/Github-icon.svg' alt="zeropool"/>
           </a>
           <a className="main-footer__social-link" href="https://medium.com/zeropool">
-            <Medium className="main-footer__social-img" />
+          <img className="main-footer__social-img" src='/img/Medium.svg' alt="zeropool"/>
           </a>
           <a className="main-footer__social-link" href="https://twitter.com/ZeroPoolNetwork">
-            <Twitter className="main-footer__social-img" />
+          <img className="main-footer__social-img" src='/img/Twitter.svg' alt="zeropool"/>
           </a>
           <a className="main-footer__social-link" href="https://t.me/ZeroPoolCommunity">
-            <Telegram className="main-footer__social-img" />
+            <img className="main-footer__social-img" src='/img/Telegram.svg' alt="zeropool"/>
           </a>
         </div>
       </div>
--- src/pages/docs.js
+++ src/pages/docs.js
@@ -12,26 +12,6 @@ const Docs = () => {
         <h1 className="text--center">Welcome to the ZeroPool Documentation Site</h1>
         <div className="row">
           <div className="col col--6">
-            <Link to="/docs/privacy-engine/" className="card card--full-height">
-              <div className="card__header">
-                <h3>Privacy Engine</h3>
-              </div>
-              <div className="card__body">
-                <p>Explore our privacy-preserving technologies</p>
-                <ul>
-                  <li>Zero-Knowledge Proofs</li>
-                  <li>Fawkes-crypto circuit builder</li>
-                </ul>
-                <div className="card__image">
-                  <img src="/img/privacy-engine.svg" alt="Privacy Engine" />
-                </div>
-              </div>
-              <div className="card__footer">
-                <button className="button button--primary">Get Started</button>
-              </div>
-            </Link>
-          </div>
-          <div className="col col--6">
             <Link to="/docs/sharded-storage" className="card card--full-height">
               <div className="card__header">
                 <h3>Sharded Storage</h3>
@@ -50,6 +30,26 @@ const Docs = () => {
               </div>
               <div className="card__footer">
                 <button className="button button--primary">Explore Docs</button>
+              </div>
+            </Link>
+          </div>
+          <div className="col col--6">
+            <Link to="/docs/privacy-engine/" className="card card--full-height">
+              <div className="card__header">
+                <h3>Privacy Engine</h3>
+              </div>
+              <div className="card__body">
+                <p>Explore our privacy-preserving technologies</p>
+                <ul>
+                  <li>Zero-Knowledge Proofs</li>
+                  <li>Fawkes-crypto circuit builder</li>
+                </ul>
+                <div className="card__image">
+                  <img src="/img/privacy-engine.svg" alt="Privacy Engine" />
+                </div>
+              </div>
+              <div className="card__footer">
+                <button className="button button--primary">Get Started</button>
               </div>
             </Link>
           </div>
